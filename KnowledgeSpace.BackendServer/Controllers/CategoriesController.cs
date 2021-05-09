4adb9d130a68768a9c8e15ad1ac39cc5cc98b997
anhtrong211998 <anhtrong211998@gmail.com>
2021-05-09 23:28:17 +0700

    Add Category API

---
+++ KnowledgeSpace.BackendServer/Controllers/CategoriesController.cs
@@ -0,0 +1,218 @@
+﻿using KnowledgeSpace.BackendServer.Models;
+using KnowledgeSpace.BackendServer.Models.Entities;
+using KnowledgeSpace.ViewModels;
+using KnowledgeSpace.ViewModels.Contents;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KnowledgeSpace.BackendServer.Controllers
+{
+    public class CategoriesController : BaseController
+    {
+        private readonly KnowledgeSpaceContext _context;
+
+        /// <summary>
+        /// CONSTRUCTOR CONTROLLER.
+        /// </summary>
+        /// <param name="context"></param>
+        public CategoriesController(KnowledgeSpaceContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// GET: api/categories
+        /// GET ALL CATEGORIES.
+        /// </summary>
+        /// <returns>HTTP STATUS WITH LIST OF CATEGORIES.</returns>
+        [HttpGet]
+        public async Task<IActionResult> GetCategories()
+        {
+            //// GET ALL CATEGORIES FROM DATABASE
+            var categorys = await _context.Categories.ToListAsync();
+
+            //// TAKE INFORMATIONS OF CATEGORY NEED SHOW AND RETURN STATUS 200
+            var categoryvms = categorys.Select(c => CreateCategoryVm(c)).ToList();
+
+            return Ok(categoryvms);
+        }
+
+        /// <summary>
+        /// GET CATEGORIES WITH KEYWORD AND PAGINATION THEM.
+        /// </summary>
+        /// <param name="filter">KEYWORD SEARCH.</param>
+        /// <param name="pageIndex">INDEX OF NEXT PAGE.</param>
+        /// <param name="pageSize">NUMBER OF RECORDS IN EACH PAGE.</param>
+        /// <returns>HTTP STATUS.</returns>
+        [HttpGet("filter")]
+        public async Task<IActionResult> GetCategoriesPaging(string filter, int pageIndex, int pageSize)
+        {
+            //// GET ALL CATEGORIES
+            var query = _context.Categories.AsQueryable();
+            //// IF KEYWORD NOT NULL, GET ALL CATEGORIES WHICH CONSTAINS KEYWORD
+            if (!string.IsNullOrEmpty(filter))
+            {
+                query = query.Where(x => x.Name.Contains(filter)
+                || x.Name.Contains(filter));
+            }
+            //// TOTAL RECORDS EQUAL NUMBER OF CATEGORIES's ROW
+            var totalRecords = await query.CountAsync();
+            //// TAKE RECORDS IN THE PAGE (NEXT PAGE)
+            var items = await query.Skip((pageIndex - 1 * pageSize))
+                .Take(pageSize).ToListAsync();
+
+            //// GIVE INFORMATIONS TO CategoryVm (JUST SHOW FIELD NEEDED)
+            var data = items.Select(c => CreateCategoryVm(c)).ToList();
+
+            //// PAGINATION
+            var pagination = new Pagination<CategoryVm>
+            {
+                Items = data,
+                TotalRecords = totalRecords,
+            };
+            return Ok(pagination);
+        }
+
+        /// <summary>
+        /// GET CATEGOR WITH ID (KEY OF CATEGORY).
+        /// </summary>
+        /// <param name="id">KEY OF CATEGORY.</param>
+        /// <returns>HTTP STATUS.</returns>
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            //// GET CATEGORY WITH ID (KEY)
+            var category = await _context.Categories.FindAsync(id);
+            //// IF KEY IS NOT EXIST (CATEGORY IS NULL), RETURN STATUS 404
+            if (category == null)
+                return NotFound();
+
+            //// GIVE INFORMATIONS TO CategoryVm (JUST SHOW FIELD NEEDED
+            CategoryVm categoryvm = CreateCategoryVm(category);
+            return Ok(categoryvm);
+        }
+
+        /// <summary>
+        /// CREATE NEW CATEGORY.
+        /// </summary>
+        /// <param name="request">INPUT DATA.</param>
+        /// <returns>HTTP STATUS.</returns>
+        [HttpPost]
+        public async Task<IActionResult> PostCategory([FromBody] CategoryCreateRequest request)
+        {
+            //// CREATE A CONSTANCE OF CATEGORY WITH INFORS ARE INPUT DATA
+            var category = new Category()
+            {
+                Name = request.Name,
+                ParentId = request.ParentId,
+                SortOrder = request.SortOrder,
+                SeoAlias = request.SeoAlias,
+                SeoDescription = request.SeoDescription
+            };
+
+            //// INSERT NEW CATEGORY INTO DATABASE AND SAVE CHANGE
+            _context.Categories.Add(category);
+            var result = await _context.SaveChangesAsync();
+
+            //// IF RESULT AFTER INSERT IS GREATER THAN 0 (TRUE), RETURN STATUS 201, ELSE RETURN STATUS 400
+            if (result > 0)
+            {
+                return CreatedAtAction(nameof(GetById), new { id = category.Id }, request);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        /// <summary>
+        /// UPDATE INFORMATIONS OF CATEGORY WITH ID (KEY).
+        /// </summary>
+        /// <param name="id">KEY OF CATEGORY.</param>
+        /// <param name="request">INPUT DATA.</param>
+        /// <returns>HTTP STATUS.</returns>
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCategory(int id, [FromBody] CategoryCreateRequest request)
+        {
+            //// GET CATEGORY WITH ID (KEY)
+            var category = await _context.Categories.FindAsync(id);
+            //// IF KEY NOT EXIST (CATEGORY IS NULL), RETURN STATUS 404
+            if (category == null)
+                return NotFound();
+
+            //// IF ID EQUAL PARENT_ID, RETURN STATUS 400 (THIS CATEGORY CANNOT BE A CHILD ITSELFT)
+            if (id == request.ParentId)
+            {
+                return BadRequest("Category cannot be a child itself.");
+            }
+
+            //// GIVE INPUT DATA FOR EACH FIELD OF OBJECT WHICH NEED UPDATE INFOMATIONS
+            category.Name = request.Name;
+            category.ParentId = request.ParentId;
+            category.SortOrder = request.SortOrder;
+            category.SeoDescription = request.SeoDescription;
+            category.SeoAlias = request.SeoAlias;
+
+            //// UPDATE CATEGORY AND SAVE CHANGE
+            _context.Categories.Update(category);
+            var result = await _context.SaveChangesAsync();
+
+            //// IF RESULT AFTER UPDATE IS GREATER THAN 0 (TRUE), RETURN STATUS 204, ELSE RETURN STATUS 400
+            if (result > 0)
+            {
+                return NoContent();
+            }
+            return BadRequest();
+        }
+
+        /// <summary>
+        /// DELETE CATEGORY WITH ID (KEY).
+        /// </summary>
+        /// <param name="id">KEY OF CATEGORY.</param>
+        /// <returns>HTTP STATUS.</returns>
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategory(string id)
+        {
+            //// GET CATEGORY WITH ID (KEY)
+            var category = await _context.Categories.FindAsync(id);
+            //// IF KEY NOT EXIST (CATEGORY IS NULL), RETURN STATUS 404
+            if (category == null)
+                return NotFound();
+
+            //// REMOVE CATEGORY FROM DATABASE AND SAVE CHANGE
+            _context.Categories.Remove(category);
+            var result = await _context.SaveChangesAsync();
+            //// IF RESULT AFTER DELETE IS GREATER THAN 0 (TRUE), RETURN HTTP STATUS 200, ELSE RETURN STATUS 400
+            if (result > 0)
+            {
+                CategoryVm categoryvm = CreateCategoryVm(category);
+                return Ok(categoryvm);
+            }
+            return BadRequest();
+        }
+
+        /// <summary>
+        /// CREATE A CONSTANCE OF CATEGORY JUST SHOW NEEDED FIELD (CategoryVm).
+        /// </summary>
+        /// <param name="category">OBJECT.</param>
+        /// <returns>CategoryVm.</returns>
+        private static CategoryVm CreateCategoryVm(Category category)
+        {
+            return new CategoryVm()
+            {
+                Id = category.Id,
+                Name = category.Name,
+                SortOrder = category.SortOrder,
+                ParentId = category.ParentId,
+                NumberOfTickets = category.NumberOfTickets,
+                SeoDescription = category.SeoDescription,
+                SeoAlias = category.SeoDescription
+            };
+        }
+    }
+}
