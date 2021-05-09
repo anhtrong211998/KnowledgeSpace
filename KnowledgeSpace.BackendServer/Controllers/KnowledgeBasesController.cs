f8c658f54329a70e3ec249d60ea7bd4e52ca7567
anhtrong211998 <anhtrong211998@gmail.com>
2021-05-10 00:17:31 +0700

    Add Comment API

--- KnowledgeSpace.BackendServer/Controllers/KnowledgeBasesController.cs
+++ KnowledgeSpace.BackendServer/Controllers/KnowledgeBasesController.cs
@@ -25,6 +25,7 @@ namespace KnowledgeSpace.BackendServer.Controllers
             _context = context;
         }
 
+        #region KNOWLEDGE BASE MANAGERMENT
         /// <summary>
         /// GET: api/knowledgebases
         /// GET ALL KNOWLEDGEBASE.
@@ -83,7 +84,7 @@ namespace KnowledgeSpace.BackendServer.Controllers
                     Title = u.Title
                 })
                 .ToListAsync();
-            
+
             //// PAGINATION
             var pagination = new Pagination<KnowledgeBaseQuickVm>
             {
@@ -109,44 +110,7 @@ namespace KnowledgeSpace.BackendServer.Controllers
                 return NotFound();
 
             //// GIVE INFO KnowledgeBaseVm (JUST SHOW NEEDED FIELDS)
-            var knowledgeBaseVm = new KnowledgeBaseVm()
-            {
-                Id = knowledgeBase.Id,
-
-                CategoryId = (int)knowledgeBase.CategoryId,
-
-                Title = knowledgeBase.Title,
-
-                SeoAlias = knowledgeBase.SeoAlias,
-
-                Description = knowledgeBase.Description,
-
-                Environment = knowledgeBase.Environment,
-
-                Problem = knowledgeBase.Problem,
-
-                StepToReproduce = knowledgeBase.StepToReproduce,
-
-                ErrorMessage = knowledgeBase.ErrorMessage,
-
-                Workaround = knowledgeBase.Workaround,
-
-                Note = knowledgeBase.Note,
-
-                OwnerUserId = knowledgeBase.OwnerUserId,
-
-                Labels = knowledgeBase.Labels,
-
-                CreateDate = knowledgeBase.CreateDate,
-
-                LastModifiedDate = knowledgeBase.LastModifiedDate,
-
-                NumberOfComments = knowledgeBase.CategoryId,
-
-                NumberOfVotes = knowledgeBase.CategoryId,
-
-                NumberOfReports = knowledgeBase.CategoryId,
-            };
+            var knowledgeBaseVm = CreateKnowledgeBaseVm(knowledgeBase);
             return Ok(knowledgeBaseVm);
         }
 
@@ -272,47 +236,247 @@ namespace KnowledgeSpace.BackendServer.Controllers
             //// IF RESULT AFTER DELETE IS GREATER THAN 0 (TRUE), RETURN STATUS 200, ELSE RETURN STATUS 400
             if (result > 0)
             {
-                var knowledgeBasevm = new KnowledgeBaseVm()
-                {
-                    Id = knowledgeBase.Id,
-
-                    CategoryId = (int)knowledgeBase.CategoryId,
-
-                    Title = knowledgeBase.Title,
-
-                    SeoAlias = knowledgeBase.SeoAlias,
-
-                    Description = knowledgeBase.Description,
-
-                    Environment = knowledgeBase.Environment,
-
-                    Problem = knowledgeBase.Problem,
-
-                    StepToReproduce = knowledgeBase.StepToReproduce,
-
-                    ErrorMessage = knowledgeBase.ErrorMessage,
-
-                    Workaround = knowledgeBase.Workaround,
-
-                    Note = knowledgeBase.Note,
-
-                    OwnerUserId = knowledgeBase.OwnerUserId,
-
-                    Labels = knowledgeBase.Labels,
-
-                    CreateDate = knowledgeBase.CreateDate,
-
-                    LastModifiedDate = knowledgeBase.LastModifiedDate,
-
-                    NumberOfComments = knowledgeBase.CategoryId,
-
-                    NumberOfVotes = knowledgeBase.CategoryId,
-
-                    NumberOfReports = knowledgeBase.CategoryId,
-                };
+                KnowledgeBaseVm knowledgeBasevm = CreateKnowledgeBaseVm(knowledgeBase);
                 return Ok(knowledgeBasevm);
             }
             return BadRequest();
         }
+
+        /// <summary>
+        /// CREATE A CONSTANCE OF KNOWLEDGE BASE JUST SHOW NEEDED FIELDS (KnowledgeBaseVm).
+        /// </summary>
+        /// <param name="knowledgeBase">OBJECT.</param>
+        /// <returns>KnowledgeBaseVm.</returns>
+        private static KnowledgeBaseVm CreateKnowledgeBaseVm(KnowledgeBase knowledgeBase)
+        {
+            return new KnowledgeBaseVm()
+            {
+                Id = knowledgeBase.Id,
+
+                CategoryId = (int)knowledgeBase.CategoryId,
+
+                Title = knowledgeBase.Title,
+
+                SeoAlias = knowledgeBase.SeoAlias,
+
+                Description = knowledgeBase.Description,
+
+                Environment = knowledgeBase.Environment,
+
+                Problem = knowledgeBase.Problem,
+
+                StepToReproduce = knowledgeBase.StepToReproduce,
+
+                ErrorMessage = knowledgeBase.ErrorMessage,
+
+                Workaround = knowledgeBase.Workaround,
+
+                Note = knowledgeBase.Note,
+
+                OwnerUserId = knowledgeBase.OwnerUserId,
+
+                Labels = knowledgeBase.Labels,
+
+                CreateDate = knowledgeBase.CreateDate,
+
+                LastModifiedDate = knowledgeBase.LastModifiedDate,
+
+                NumberOfComments = knowledgeBase.NumberOfComments,
+
+                NumberOfVotes = knowledgeBase.NumberOfVotes,
+
+                NumberOfReports = knowledgeBase.NumberOfReports
+            };
+        }
+        #endregion
+
+        #region COMMENT MANAGERMENT
+        /// <summary>
+        /// GET COMMENTS OF KNOWLEDGE BASE WITH FILTER (KEYWORD SEARCH).
+        /// </summary>
+        /// <param name="knowledgeBaseId">KEY OF KNOWLEDGE BASE.</param>
+        /// <param name="filter">KEYWORD SEARCH.</param>
+        /// <param name="pageIndex">INDEX OF NEXT PAGE.</param>
+        /// <param name="pageSize">NUMBER OF RECORDS IN EACH PAGE.</param>
+        /// <returns>HTTP STATUS.</returns>
+        [HttpGet("{knowledgeBaseId}/comments/filter")]
+        public async Task<IActionResult> GetCommentsPaging(int knowledgeBaseId, string filter, int pageIndex, int pageSize)
+        {
+            //// GET ALL COMMENTS OF KNOWLEDGE BASE
+            var query = _context.Comments.Where(x => x.KnowledgeBaseId == knowledgeBaseId).AsQueryable();
+            //// IF KEYWORD NOT NULL OR EMPTY, GET ALL COMMENTS WHICH CONSTAINS KEYWORD
+            if (!string.IsNullOrEmpty(filter))
+            {
+                query = query.Where(x => x.Content.Contains(filter));
+            }
+
+            //// TOTAL RECORDS EQUAL NUMBER OF COMMENTS's ROWS
+            var totalRecords = await query.CountAsync();
+
+            //// TAKE RECORDS IN THE PAGE (NEXT PAGE) AND GIVE INFORMATIONS TO CommentVm (JUST SHOW FIELD NEEDED)
+            var items = await query.Skip((pageIndex - 1 * pageSize))
+                .Take(pageSize)
+                .Select(c => new CommentVm()
+                {
+                    Id = c.Id,
+                    Content = c.Content,
+                    CreateDate = c.CreateDate,
+                    KnowledgeBaseId = c.KnowledgeBaseId,
+                    LastModifiedDate = c.LastModifiedDate,
+                    OwnwerUserId = c.OwnwerUserId
+                })
+                .ToListAsync();
+            //// PAGINATION
+            var pagination = new Pagination<CommentVm>
+            {
+                Items = items,
+                TotalRecords = totalRecords,
+            };
+            return Ok(pagination);
+        }
+
+        /// <summary>
+        /// GET COMMENT DETAIL.
+        /// </summary>
+        /// <param name="commentId">KEY OF COMMENT.</param>
+        /// <returns>HTTP STATUS WITH COMMENT's INFORMATION.</returns>
+        [HttpGet("{knowledgeBaseId}/comments/{commentId}")]
+        public async Task<IActionResult> GetCommentDetail(int commentId)
+        {
+            //// GET COMMENT WITH ID (KEY)
+            var comment = await _context.Comments.FindAsync(commentId);
+            //// IF KEY NOT EXIST (COMMENT IS NULL), RETURN STATUS 404
+            if (comment == null)
+                return NotFound();
+
+            //// CREATE A CONSTANCE OF COMMENT JUST SHOW NEEDED FIELD
+            var commentVm = new CommentVm()
+            {
+                Id = comment.Id,
+                Content = comment.Content,
+                CreateDate = comment.CreateDate,
+                KnowledgeBaseId = comment.KnowledgeBaseId,
+                LastModifiedDate = comment.LastModifiedDate,
+                OwnwerUserId = comment.OwnwerUserId
+            };
+
+            return Ok(commentVm);
+        }
+
+        /// <summary>
+        /// CREATE NEW COMMENT.
+        /// </summary>
+        /// <param name="knowledgeBaseId">KEY OF KNOWLEDGE BASE.</param>
+        /// <param name="request">INPUT DATA.</param>
+        /// <returns>HTTP STATUS.</returns>
+        [HttpPost("{knowledgeBaseId}/comments")]
+        public async Task<IActionResult> PostComment(int knowledgeBaseId, [FromBody] CommentCreateRequest request)
+        {
+            //// CREATE NEW CONSTANCE OF COMMENT WITH INFORMATION ARE INPUT DATA
+            var comment = new Comment()
+            {
+                Content = request.Content,
+                KnowledgeBaseId = request.KnowledgeBaseId,
+                OwnwerUserId = string.Empty/*TODO: GET USER FROM CLAIM*/,
+            };
+
+            //// INSERT NEW COMMENT
+            _context.Comments.Add(comment);
+            //// GET KNOWLEDGE BASE WITH ID (KEY), IF KEY NOT EXIST RETURN STATUS 404
+            var knowledgeBase = await _context.KnowledgeBases.FindAsync(knowledgeBaseId);
+            if (knowledgeBase == null)
+                return BadRequest();
+
+            //// UPDATE NUMBER OF COMMENTS INCREASE 1 AND SAVE CHANGE
+            knowledgeBase.NumberOfComments = knowledgeBase.NumberOfVotes.GetValueOrDefault(0) + 1;
+            _context.KnowledgeBases.Update(knowledgeBase);
+            var result = await _context.SaveChangesAsync();
+
+            //// IF RESULT AFTER INSERT IS GREATER THAN 0 (TRUE), RETURN STATUS 201, ELSE RETURN STATUS 400
+            if (result > 0)
+            {
+                return CreatedAtAction(nameof(GetCommentDetail), new { id = knowledgeBaseId, commentId = comment.Id }, request);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        /// <summary>
+        /// UPDATE COMMENT WITH ID (KEY).
+        /// </summary>
+        /// <param name="commentId">KEY OF COMMENT.</param>
+        /// <param name="request">INPUT DATA.</param>
+        /// <returns>HTTP STATUS.</returns>
+        [HttpPut("{knowledgeBaseId}/comments/{commentId}")]
+        public async Task<IActionResult> PutComment(int commentId, [FromBody] CommentCreateRequest request)
+        {
+            //// GET COMMENT WITH ID (KEY)
+            var comment = await _context.Comments.FindAsync(commentId);
+            //// IF KEY IS NOT EXIST (COMMENT IS NULL), RETURN STATUS 404
+            if (comment == null)
+                return NotFound();
+            //// IF OWNWER USER DIFFERENT CURRENT USER, RETURN STATUS 403
+            if (comment.OwnwerUserId != User.Identity.Name)
+                return Forbid();
+
+            //// UPDATE INFORMATION AND SAVE CHANGE
+            comment.Content = request.Content;
+            _context.Comments.Update(comment);
+            var result = await _context.SaveChangesAsync();
+
+            //// IF RESULT AFTER UPDATE IS GREATER THAN 0 (TRUE), RETURN STATUS 204, ELSE RETURN STATUS 400
+            if (result > 0)
+            {
+                return NoContent();
+            }
+            return BadRequest();
+        }
+
+        /// <summary>
+        /// DELETE COMMENT WITH ID (KEY).
+        /// </summary>
+        /// <param name="knowledgeBaseId">KEY OF KNOWLEDGE BASE.</param>
+        /// <param name="commentId">KEY OF COMMENT.</param>
+        /// <returns>HTTP STATUS.</returns>
+        [HttpDelete("{knowledgeBaseId}/comments/{commentId}")]
+        public async Task<IActionResult> DeleteComment(int knowledgeBaseId, int commentId)
+        {
+            //// GET COMMENT WITH ID (KEY), IF KEY IS NOT EXIST, RETURN STATUS 404
+            var comment = await _context.Comments.FindAsync(commentId);
+            if (comment == null)
+                return NotFound();
+
+            //// REMOVE COMMENT
+            _context.Comments.Remove(comment);
+
+            //// GET KNOWLEDGE BASE WITH ID, IF NULL, RETURN STATUS 400
+            var knowledgeBase = await _context.KnowledgeBases.FindAsync(knowledgeBaseId);
+            if (knowledgeBase == null)
+                return BadRequest();
+
+            //// UPDATE NUMBER OF COMMENT IS DECREASE 1 AND SAVE CHANGE
+            knowledgeBase.NumberOfComments = knowledgeBase.NumberOfVotes.GetValueOrDefault(0) - 1;
+            _context.KnowledgeBases.Update(knowledgeBase);
+            var result = await _context.SaveChangesAsync();
+
+            //// IF RESULT AFTER DELETE IS GREATER THAN 0 (TRUE), RETURN STATUS 200, ELSE RETURN STATUS 400
+            if (result > 0)
+            {
+                var commentVm = new CommentVm()
+                {
+                    Id = comment.Id,
+                    Content = comment.Content,
+                    CreateDate = comment.CreateDate,
+                    KnowledgeBaseId = comment.KnowledgeBaseId,
+                    LastModifiedDate = comment.LastModifiedDate,
+                    OwnwerUserId = comment.OwnwerUserId
+                };
+                return Ok(commentVm);
+            }
+            return BadRequest();
+        }
+        #endregion
     }
 }
---
+++ KnowledgeSpace.ViewModels/Validators/KnowledgeBaseCreateRequestValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using KnowledgeSpace.ViewModels.Contents;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KnowledgeSpace.ViewModels.Validators
+{
+    public class KnowledgeBaseCreateRequestValidator : AbstractValidator<KnowledgeBaseCreateRequest>
+    {
+        public KnowledgeBaseCreateRequestValidator()
+        {
+        }
+    }
+}
