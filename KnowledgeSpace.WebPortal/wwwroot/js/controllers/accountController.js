5cbd9a6c01013a4e12f443b6323fb768892a0acb
anhtrong211998 <anhtrong211998@gmail.com>
2021-06-20 00:05:17 +0700

    update layout

--- KnowledgeSpace.WebPortal/wwwroot/js/controllers/accountController.js
+++ KnowledgeSpace.WebPortal/wwwroot/js/controllers/accountController.js
@@ -10,9 +10,39 @@
         //CKEDITOR.replace('txt_workaround');
         CKEDITOR.replace('txt_note');
         //CKEDITOR.replace('txt_description');
-
         $('#btn_add_attachment').off('click').on('click', function () {
-            $('#attachment_items').prepend('<p><input type="file" name="attachments" /></p>');
+
+            //// declare html variable includes html that need render on elemet with renderHTMLAnswer class
+            var html = '';
+            html += '<div class="input-group">';
+            html += '<div class="input-group-area">';
+            html += '<input type="text" class="form-control txtAttachment" placeholder="Type your attachment" name="txtAttachment">';
+            html += '</div>'
+            html += '<div class="input-group-append">';
+            html += '<button class="btn btn-info btnAttachment" type="button">Browser</button>';
+            html += '</div>'
+            html += '<div class="input-group-append">';
+            html += '<button class="btn btn-info btn_remove_attachment" type="button" style="width:33px;">-</button>';
+            html += '</div>';
+            html += '<p><input type="file" class="selectFile" name="attachments" style="display:none;" /></p>';
+            html += ' </div>';
+
+            //// render html inside element with class="renderHTMLAnswers"
+            $('#render-attachment').prepend(html);
+            return false;
+        });
+
+        $('body').on('click', '.btn_remove_attachment', function () {
+            $(this).parent().parent().remove();
+        });
+
+        $('body').on('click', '.btnAttachment', function (){
+            if ($(this).parent().parent().find($('input[name="attachments"]')).click()) {
+                $(this).parent().parent().find($('input[name="attachments"]')).on('change', function (evt) {
+                    //// set value of input with id="ChooseCV" is name of file
+                    $(this).parent().parent().find($('input[name="txtAttachment"]')).val(evt.target.files[0].name);
+                });
+            }
             return false;
         });
 
